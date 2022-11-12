1a27b347daa1b6a7f1e2d57a44d58ae3ab584e94
HyeS00 <hyesookim1118@gmail.com>
2022-11-13 04:26:45 +0900

    [feat] add routeDetailTableView

---
+++ WidgetBus/Views/RouteDetailView/RouteDetailTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RouteDetailTableViewCell.swift
+//  WidgetBus
+//
+//  Created by 김혜수 on 2022/11/12.
+//
+
+import UIKit
+
+class RouteDetailTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var routePointImageView: UIImageView!
+    @IBOutlet weak var routeLineView: UIView!
+    @IBOutlet weak var busStationLabel: UILabel!
+}
---
+++ WidgetBus/Views/RouteDetailView/RouteDetailViewController.swift
@@ -0,0 +1,58 @@
+//
+//  RouteDetailViewController.swift
+//  WidgetBus
+//
+//  Created by 김혜수 on 2022/11/12.
+//
+
+import UIKit
+
+class RouteDetailViewController: UIViewController {
+
+    @IBOutlet weak var busNumberLabel: UILabel!
+    @IBOutlet weak var startStationLabel: UILabel!
+    @IBOutlet weak var endStationLabel: UILabel!
+    @IBOutlet weak var busTimeInfoLabel: UILabel!
+
+    @IBOutlet weak var routeView: UIView!
+    @IBOutlet weak var routeDetailTableView: UITableView!
+
+    @IBOutlet weak var boardingStateButton: UIButton!
+    override func viewDidLoad() {
+
+        routeView.clipsToBounds = true
+        routeView.layer.cornerRadius = 30
+        routeView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+
+        self.routeDetailTableView.dataSource = self
+        self.routeDetailTableView.delegate = self
+        busNumberLabel.text = "1000"
+
+    }
+}
+
+extension RouteDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "routeDetailCell", for: indexPath) as! RouteDetailTableViewCell
+        cell.busStationLabel.text = "포스텍"
+        cell.routeLineView.backgroundColor = .gray
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        UIView()
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+}
+
+extension RouteDetailViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
