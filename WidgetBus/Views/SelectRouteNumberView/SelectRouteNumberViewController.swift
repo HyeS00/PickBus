37a22f7e106e58f45bd2f9a694be621c9ff76b73
Jedi <shwoghk14@gmail.com>
2022-11-13 16:30:23 +0900

    [Feat] Create SelectRouteNumberViewController
    #14 버스 번호 선택 뷰 컨트롤러 생성 및 데이터 소스 연결.

---
+++ WidgetBus/Views/SelectRouteNumberView/SelectRouteNumberViewController.swift
@@ -0,0 +1,71 @@
+//
+//  SelectRouteNumberViewController.swift
+//  WidgetBus
+//
+//  Created by Jaehwa Noh on 2022/11/10.
+//
+
+import UIKit
+
+class SelectRouteNumberViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    //
+    private var routeNumberInfos = [ArriveInfoResponseArriveInfo]()
+    //
+    @IBOutlet weak var routeNumberTableView: UITableView!
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+        }
+        //
+        self.navigationItem.title = "송강전통시장"
+        BusClient.getArriveList(completion: handleRequestArriveInfoResponse(response:error:))
+    }
+    //
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RouteNumberCell", for: indexPath)
+        //
+        var cellContent = cell.defaultContentConfiguration()
+        if routeNumberInfos.isEmpty {
+            cellContent.text = "불러오는 중입니다."
+//            cell.tintColor = .gray
+            //
+        } else {
+            let cellData = routeNumberInfos[indexPath.row]
+            cellContent.text = "\(cellData.routeno) 번"
+            cellContent.secondaryText = cellData.routetp
+            //
+        }
+        if selectedIndex == indexPath {
+            cell.backgroundColor = UIColor.blue
+        } else {
+            cell.backgroundColor = UIColor.clear
+        }
+        cell.contentConfiguration = cellContent
+        //
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("run \(routeNumberInfos)")
+        if routeNumberInfos.isEmpty {
+            return 3
+        } else {
+            return routeNumberInfos.count
+        }
+        //
+    }
+
+    func handleRequestArriveInfoResponse(response: [ArriveInfoResponseArriveInfo], error: Error?) {
+        routeNumberInfos = response
+        routeNumberTableView.reloadData()
+        for test in response {
+            print(test)
+        }
+        print("response")
+        //        print(response)
+        print("error")
+        print(error?.localizedDescription ?? "")
+    }
+//
+}
