af76c28e1e6684d2b974d8b00d80336cb67967bf
Shin-jun <inside1451@naver.com>
2022-11-13 22:53:26 +0900

    [feat] #9 add initMainView
    메인뷰 초기화면 구현하였습니다.

---
+++ WidgetBus/View/GroupListViewContoller/AddGroupListNameView.swift
@@ -0,0 +1,16 @@
+//
+//  AddGroupListNameView.swift
+//  WidgetBus
+//
+//  Created by Shin yongjun on 2022/11/13.
+//
+
+import UIKit
+
+final class AddGroupListNameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+
+    }
+}
--- WidgetBus/View/GroupListViewContoller/GroupListViewContoller.swift
+++ WidgetBus/View/GroupListViewContoller/GroupListViewContoller.swift
@@ -12,11 +12,9 @@ final class GroupListViewContoller: UIViewController {
         super.viewDidLoad()
 
         setupNavigationController()
+        setupMainView()
     }
 }
-
-
-
 
 private extension GroupListViewContoller {
     func setupNavigationController() {
@@ -33,13 +31,52 @@ private extension GroupListViewContoller {
         button.tintColor = .black
         let barButtonItem = UIBarButtonItem(customView: button)
         barButtonItem.customView?.translatesAutoresizingMaskIntoConstraints = false
-        barButtonItem.customView?.heightAnchor.constraint(equalToConstant: 24).isActive = true
-        barButtonItem.customView?.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        barButtonItem.customView?.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        barButtonItem.customView?.widthAnchor.constraint(equalToConstant: 40).isActive = true
         self.navigationItem.rightBarButtonItem = barButtonItem
     }
 
     @objc func pressButton(_ sender: UIBarButtonItem) {
         let settingView = SettingViewController()
         self.navigationController?.pushViewController(settingView, animated: true)
+    }
+}
+
+private extension GroupListViewContoller {
+    func setupMainView() {
+        let mainLabel = UILabel()
+        mainLabel.text = "그룹을 \n추가해주세요."
+        mainLabel.numberOfLines = 2
+        mainLabel.textAlignment = .center
+        mainLabel.font = UIFont.systemFont(ofSize: 20, weight: .light)
+        self.view.addSubview(mainLabel)
+
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false
+        mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mainLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        let addButton = UIButton()
+        addButton.setTitle("추가하기", for: .normal)
+        addButton.setTitleColor(.white, for: .normal)
+        addButton.backgroundColor = .blue
+        addButton.layer.opacity = 0.3
+        addButton.layer.cornerRadius = 15
+        addButton.layer.shadowColor = UIColor.black.cgColor
+        addButton.layer.shadowOpacity = 1.0
+        addButton.layer.shadowOffset = CGSize.zero
+        addButton.layer.shadowRadius = 2
+        self.view.addSubview(addButton)
+
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        addButton.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 70).isActive = true
+        addButton.centerXAnchor.constraint(equalTo: mainLabel.centerXAnchor).isActive = true
+        addButton.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        addButton.widthAnchor.constraint(equalToConstant: 361).isActive = true
+        addButton.addTarget(self, action: #selector(btnAddGroupList(_:)), for: .touchUpInside)
+    }
+
+    @objc func btnAddGroupList(_ sender: UIButton) {
+        let addGroupListNameView = AddGroupListNameViewController()
+        self.navigationController?.pushViewController(addGroupListNameView, animated: true)
     }
 }
