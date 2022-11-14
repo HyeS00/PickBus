2e15e053422642efde19e6ffeffc35f316506918
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2022-11-14 16:23:03 +0900

    [feat] #28 add tableView cell
    클래스 명 : ArrivalTableViewCell

---
+++ WidgetBus/Views/SelectArrivalView/ArrivalTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ArrivalTableViewCell.swift
+//  WidgetBus
+//
+//  Created by LeeJaehoon on 2022/11/14.
+//
+
+import UIKit
+
+class ArrivalTableViewCell: UITableViewCell {
+    // MARK: - Properties
+
+    // MARK: - Lifecycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
