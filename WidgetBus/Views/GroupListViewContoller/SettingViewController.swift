093b729ea1fb80e27bb8286c1aa0c94cccf2904f
Shin-jun <inside1451@naver.com>
2022-11-14 16:42:05 +0900

    [feat] settingView에 navigation 추가
    settingView에 네비게이션바 추가했습니다.

--- WidgetBus/Views/GroupListViewContoller/SettingViewController.swift
+++ WidgetBus/Views/GroupListViewContoller/SettingViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 final class SettingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        setupNavigationController()
 
+    }
+}
+private extension SettingViewController {
+    func setupNavigationController() {
+        let button = UIButton(type: .system)
+        let barButtonItem = UIBarButtonItem(customView: button)
+        barButtonItem.customView?.translatesAutoresizingMaskIntoConstraints = false
+        barButtonItem.customView?.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        barButtonItem.customView?.widthAnchor.constraint(equalToConstant: 40).isActive = true
+
+        self.navigationItem.rightBarButtonItem = barButtonItem
+        self.navigationController?.navigationBar.tintColor = .black
+        self.navigationController?.navigationBar.topItem?.title = ""
     }
 }
