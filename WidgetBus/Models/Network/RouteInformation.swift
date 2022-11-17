21e547051605f01db0124a0d0c3d58e2e3b34f74
Jedi <shwoghk14@gmail.com>
2022-11-17 15:16:58 +0900

    [Feat] Create Route Information Network Structure
    #33 노선 정보 용 구조체 생성.

--- WidgetBus/Models/Network/RouteInformation.swift
+++ WidgetBus/Models/Network/RouteInformation.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+struct RouteInformation: Codable {
+    let response: RouteInformationResponse
+}
+
+struct RouteInformationResponse: Codable {
+    let header: RouteInformationResponseHeader
+    let body: RouteInformationResponseBody
+}
+
+struct RouteInformationResponseHeader: Codable {
+    let resultCode: String
+    let resultMsg: String
+}
+
+struct RouteInformationResponseBody: Codable {
+    let items: [RouteInformationResponseItem]
+}
+
+struct RouteInformationResponseItem: Codable {
+    // 종점
+    let endnodenm: String
+    // 막차 시간
+    let endvehicletime: Int
+    // 배차 간격(토요일)
+    let intervalsattime: Int
+    // 배차 간격(일요일)
+    let intervalsuntime: Int
+    // 배차 간격(평일)
+    let intervaltime: Int
+    // 노선 ID
+    let routeid: String
+    // 노선 번호
+    let routeno: Int
+    // 노선 유형(마을 버스 등)
+    let routetp: String
+    // 기점
+    let startnodenm: String
+    // 첫차 시간
+    let startvehicletime: String
+}
