025f5b72089934758caa8c50b8c7d21dc691b70b
Jedi <shwoghk14@gmail.com>
2022-11-20 19:04:00 +0900

    [Feat] Add search node function
    #40 url 추가

--- WidgetBus/Models/Network/BusClient.swift
+++ WidgetBus/Models/Network/BusClient.swift
@@ -28,6 +28,7 @@ class BusClient {
         case getArriveList(city: String, busStopId: String)
         case getCityCodeList
         case getNodesList(city: String, routeId: String, pageNumber: String = "1")
+        case searchNode(city: String, nodeNm: String?, nodeNo: String?)
 
         var stringValue: String {
             switch self {
@@ -49,6 +50,29 @@ class BusClient {
                 "&_type=json&cityCode=\(city)&routeId=\(routeId)&numOfRows=99"
                 +
                 "&pageNo=\(pageNumber)"
+
+            case .searchNode(let city, let nodeNm, let nodeNo):
+                var url: String = ""
+                if let nodeNm = nodeNm {
+                    let escapedNodeNm = nodeNm.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+                    url = Endpoints.base +
+                    "/1613000/BusSttnInfoInqireService/getSttnNoList" +
+                    Endpoints.apiKeyParam +
+                    "&_type=json&cityCode=\(city)&nodeNm=\(escapedNodeNm ?? "")" +
+                    "&numOfRows=99"
+                } else {
+                    if let nodeNo = nodeNo {
+                        url = Endpoints.base +
+                        "/1613000/BusSttnInfoInqireService/getSttnNoList" +
+                        Endpoints.apiKeyParam +
+                        "&_type=json&cityCode=\(city)&nodeNo=\(nodeNo)" +
+                        "&numOfRows=99"
+                    } else {
+                        url = ""
+                    }
+                }
+//                print("url: \(url)")
+                return url
             }
         }
 
@@ -72,7 +96,7 @@ class BusClient {
             }
             let decoder = JSONDecoder()
             do {
-//                print(String(decoding: data, as: UTF8.self))
+                //                print(String(decoding: data, as: UTF8.self))
                 let responseObject = try decoder.decode(ResponseType.self, from: data)
                 DispatchQueue.main.async {
                     completion(responseObject, nil)
@@ -86,6 +110,59 @@ class BusClient {
         task.resume()
 
         return task
+    }
+
+    class func taskForGetRequestWithCityCode<ResponseType: Decodable>(
+        url: URL,
+        cityCode: Int,
+        responseType: ResponseType.Type,
+        completion: @escaping (Int?, ResponseType?, Error?) -> Void
+    ) -> URLSessionDataTask {
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print("data is nil")
+                DispatchQueue.main.async {
+                    completion(nil, nil, error)
+                }
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                //                print(String(decoding: data, as: UTF8.self))
+                let responseObject = try decoder.decode(ResponseType.self, from: data)
+                DispatchQueue.main.async {
+                    completion(cityCode, responseObject, nil)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(nil, nil, error)
+                }
+            }
+        }
+        task.resume()
+
+        return task
+    }
+
+    class func searchNodeList(
+        city: String = "25",
+        nodeNm: String? = "전통시장",
+        nodeNo: String? = "44810",
+        completion: @escaping (Int?, [SearchNodeInfo], Error?) -> Void
+    ) {
+        _ = taskForGetRequestWithCityCode(
+            url: Endpoints.searchNode(
+                city: city,
+                nodeNm: nodeNm,
+                nodeNo: nodeNo).url,
+            cityCode: Int(city) ?? 0,
+            responseType: SearchNode.self) { city, response, error in
+                if let response = response {
+                    completion(city, response.response.body.items.item.listValue, nil)
+                } else {
+                    completion(nil, [], error)
+                }
+            }
     }
 
     class func getArriveList(
@@ -126,6 +203,19 @@ class BusClient {
             _ = taskForGETRequest(
                 url: Endpoints.getNodesList(city: city, routeId: routeId, pageNumber: pageNo).url,
                 responseType: RouteNodes.self) { response, error in
+                    if let response = response {
+                        completion(response.response.body.items.item, nil)
+                    } else {
+                        completion([], error)
+                    }
+                }
+        }
+
+    class func getCityCodeList (
+        completion: @escaping ([CityCodeInfo], Error?) -> Void) {
+            _ = taskForGETRequest(
+                url: Endpoints.getCityCodeList.url,
+                responseType: CityCode.self) { response, error in
                     if let response = response {
                         completion(response.response.body.items.item, nil)
                     } else {
