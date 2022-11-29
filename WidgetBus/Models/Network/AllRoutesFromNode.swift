a76d5bd92e18e3b8200b2f9ec59a5d874640e9c2
Jedi <shwoghk14@gmail.com>
2022-11-29 16:24:42 +0900

    [Feat] Create Multitypes
    #57

--- WidgetBus/Models/Network/AllRoutesFromNode.swift
+++ WidgetBus/Models/Network/AllRoutesFromNode.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+struct AllRoutesFromNode: Decodable {
+    let response: AllRoutesFromNodeResponseAPI
+}
+
+struct AllRoutesFromNodeResponseAPI: Decodable {
+    let header: AllRoutesFromNodeResponseHeader
+    let body: AllRoutesFromNodeResponseBody
+}
+
+struct AllRoutesFromNodeResponseHeader: Decodable {
+    let resultCode: String
+    let resultMsg: String
+}
+
+struct AllRoutesFromNodeResponseBody: Decodable {
+    let items: AllRoutesFromNodeItem
+    let numOfRows: Int
+    let pageNo: Int
+    let totalCount: Int
+}
+
+struct AllRoutesFromNodeItem: Decodable {
+    let item: MyAllRoutesFromNodeMultiType
+}
+
+struct AllRoutesFromNodeInfo: Decodable {
+    /// 도착예정버스 남은 정류장 수
+    let arrprevstationcnt: Int
+    /// 도착예정버스 도착예정 시간[초]
+    let arrtime: Int
+    /// 정류소 ID
+    let nodeid: String
+    /// 정류소 명
+    let nodenm: String
+    /// 노선 ID
+    let routeid: String
+    /// 노선 번호
+    let routeno: StringMultiType
+    /// 노선 유형(마을 버스 등)
+    let routetp: String
+    /// 도착예정버스 차량유형(저상 버스 등)
+    let vehicletp: String
+}
