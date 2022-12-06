dd2c954df30a6da5cd20fafa49aaafcb27220065
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2022-12-07 02:16:08 +0900

    [feat] add transit animation file #74
    트랜지션 애니메이션 파일 추가

---
+++ WidgetBus/CustomAnimation/ExtendFromCellTransition.swift
@@ -0,0 +1,8 @@
+//
+//  ExtendFromCellTransition.swift
+//  WidgetBus
+//
+//  Created by LeeJaehoon on 2022/12/07.
+//
+
+import Foundation
---
+++ WidgetBus/CustomAnimation/ShrinkToCellTransition.swift
@@ -0,0 +1,8 @@
+//
+//  ShrinkToCellTransition.swift
+//  WidgetBus
+//
+//  Created by LeeJaehoon on 2022/12/07.
+//
+
+import Foundation
