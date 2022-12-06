7d1b520c08dcade719fae0e56c953e968100cec0
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2022-12-07 01:58:17 +0900

    [feat] Add TransitInfoProtocol #74
    정보를 가져오기 위해 구현

---
+++ WidgetBus/CustomAnimation/TransitInfoProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  TransitInfoProtocol.swift
+//  WidgetBus
+//
+//  Created by LeeJaehoon on 2022/12/07.
+//
+
+import UIKit
+
+protocol TransitInfoProtocol {
+    var view: UIView! { get set }
+
+    func getCellFrame() -> CGRect?
+
+    func getCell() -> UITableViewCell?
+}
+
